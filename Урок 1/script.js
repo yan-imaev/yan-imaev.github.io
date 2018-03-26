bb994a3f89bfeb4c79691bb62ddfce2a3905d60e
yan-imaev <yan.imaev@gmail.com>
2018-03-26 23:10:30 +0500

    Add files via upload

---
+++ Урок 1/script.js
@@ -0,0 +1,37 @@
+obj = {
+	name: "John",
+	age: 25,
+	isMarried: false
+}
+
+var arr = ['pulm', 'orange', 'apple'];
+//console.log(arr[1]);
+
+//alert("Hello");
+//confirm("are u here?");
+
+//var answer = prompt("Вам есть 18?", "18");
+
+//console.log( typeof(arr));
+
+//console.log( arr[2] + " - object");
+//console.log( 4 + " - object");
+
+var incr = 10,
+	decr = 10;
+
+	//incr++;
+	//decr--;
+
+	//++incr;
+	//--decr;
+
+	console.log(incr);
+	console.log(decr);
+
+console.log(incr == 10);
+
+var isChecked = true;
+var isClose = true;
+
+console.log(isChecked || isClose);
